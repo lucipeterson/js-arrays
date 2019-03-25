704bd90f79f8d38fc36fb3fee9556bbfb08dae1b
Luci Peterson <lucijpeterson@gmail.com>
2019-03-24 20:45:34 -0700

    Created for loops that generate arrays
    Created for loops that generate arrays of random numbers and print them to the console.

--- practice_js_arrays/starting_code/2_loop.js
+++ practice_js_arrays/starting_code/2_loop.js
@@ -13,12 +13,24 @@ function random100() {
 }
 
 /* 1. Create a function named createRandomList that uses a for loop to create an array containing 10 random numbers from 1 to 100 (use the supplied function above to generate the numbers). The function should return that array. */
+let myRandomList = [];
 
+function createRandomList() {
+  for (let i = 0; i < 10; i += 1) {
+    let randomNum = random100();
+    myRandomList.push(randomNum);
+  }
+}
+
+createRandomList();
+console.log(myRandomList);
 /* 2. Call the createRandomList() function and store the results in a variable named myRandomList. */
 
 /* 3. Use a for loop to access each element in the loop. Each time through the loop log a message to the console that looks something like this:
 Item 0 in the array is 48
 When you're done you should have output 10 lines to the console -- one for each element. 
 */
-
+for (i = 0; i < myRandomList.length; i += 1){
+  console.log("Item " + i + " in the array is " + myRandomList[i]);
+}
 // Run your code by typing node 2_loop.js in the console below
