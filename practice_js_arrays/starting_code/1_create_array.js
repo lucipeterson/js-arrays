e219c5355c75ea043ba9d23c9aebfd976871c6d5
Luci Peterson <lucijpeterson@gmail.com>
2019-03-24 19:46:37 -0700

    Create & manipulate an array
    Created an array of programming languages and manipulated using various array methods.

--- practice_js_arrays/starting_code/1_create_array.js
+++ practice_js_arrays/starting_code/1_create_array.js
@@ -8,23 +8,23 @@ If your program is stuck in an infinite loop, you can break out of the program b
 */
 
 // 1. Create a new array named languages that contains at least 7 programming languages.
-
+let languages = ["JavaScript", "Ruby", "Python", "Java", "C", "PHP", "Swift"];
 // 2. Use console.log to display the number of elements in the array:
-
+console.log(languages.length);
 /* 3. Use console.log to list the first element from the array using an index value. */
-
+console.log(languages[0]);
 /* 4. Use console.log to list the last element from the array using an index value. */
-
+console.log(languages[6]);
 // 5. Use an array method to add an element to the END of the array.
-
+languages.push("SQL");
 // 6. Use an array method to add an element to the BEGINNING of the array.
-
+languages.unshift("C-Sharp");
 /* 7. Log all of the elements in the array as a single string using the array .join() method */
-
+languages.join(", ");
 // 8. Use an array method to REMOVE the LAST element from the array.
-
+languages.pop();
 // 9. Use an array method to REMOVE the first element from the array.
-
+languages.shift();
 /* 10. Log all of the elements in the array as a single string using the array .join() method. Thisis the same as step 7 above */
-
+languages.join(", ");
 // Run your code by typing node 1_create_array.js in the console below
